036bee523d1649127995e5c3eeb29f39c18cce85
Hristijan <158057428+hanastas1@users.noreply.github.com>
2025-02-10 15:19:25 +0100

    Release 2.5.20

--- Package.swift
+++ Package.swift
@@ -17,8 +17,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(
             name: "ThreeDS_SDK",
-            url: "https://merchant-plug-in.extranet.netcetera.biz/nca-348-1/readonly-dav/3dssdk-release/ios/release/2.5.11/ThreeDS_SDK.zip",
-            checksum: "4331fc7fdd1b70a29c978329ddc2bdd2f1b81e5f99394d066ac39cb5d57e3538"
+            url: "https://merchant-plug-in.extranet.netcetera.biz/nca-348-1/readonly-dav/3dssdk-release/ios/release/2.5.20/ThreeDS_SDK.zip",
+            checksum: "aa39ae1a1b7860e970b7c1eafda522b8942a190d86bda65ce52a7c3ab9413931"
         )
     ]
 )
